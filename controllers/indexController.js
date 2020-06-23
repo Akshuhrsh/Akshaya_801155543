a93f7e356c9ced3a773ad45b865292d7d90de5a1
akshu <easwaranakshaya@gmail.com>
2020-06-23 02:32:23 -0400

    retrieving and showing post from DB

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,12 +1,34 @@
+const sqllite = require('sqlite3').verbose();
 
+let db = new sqllite.Database('nodejsdb.db', (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+  });
 
+  let sql = `SELECT title title,desc desc FROM BLOG
+           ORDER BY id DESC LIMIT 4`;
 
-var data = [{title: 'Blog 1', desc: 'Desc1 goes here'},{title: 'Blog 2', desc: 'Desc2 goes here'},{title: 'Blog 3', desc: 'Desc3 goes here'}];
+ 
 
 module.exports = function(app){
+    var data=[];
 
     app.get('/index',function(req,res){
-        res.render('index',{blogs: data});
+        db.all(sql, [], (err, rows) => {
+            if (err) {
+              throw err;
+            }
+            rows.forEach((row) => {
+                console.log("inside for each");               
+              console.log(row.title + row.desc);
+             var blog= {title:row.title,desc:row.desc};
+              console.log(data.push(blog)); 
+              console.log(data);
+            });
+            res.render('index',{blogs: data});
+          });  
     }); 
 };
     
